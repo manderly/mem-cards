4c7e41cf50c800d12092ca8dfe19176e06a4c8c3
Mandi <mandi018@gmail.com>
2023-08-22 21:51:19 -0500

    Exposed host on local network for testing on phone, minor style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -56,11 +56,9 @@ function App() {
     <>
     <div className="app-container">
       <h1>{glyph}</h1>
-      <div className="card">
         <Button variant="contained" size="large" className="heckinButton" onClick={() => generateRandomGlyph()}>
           Generate a random glyph
         </Button>
-      </div>
 
       <br/>
     </div>
