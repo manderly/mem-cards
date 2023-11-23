5434761a938ec0bb54b57b89640b5f9347e3a56e
Mandi <mandi018@gmail.com>
2023-11-23 09:00:04 -0600

    Removed button and made the card itself tappable, made words a little larger, added November 2023 words

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import {Box, Button, FormControlLabel, Switch, Tab, Tabs} from "@mui/material";
+import {Box, FormControlLabel, Switch, Tab, Tabs} from "@mui/material";
 import { data } from './assets/data';
 
 function App() {
@@ -9,17 +9,19 @@ function App() {
   const [includeLowercase, setIncludeLowercase] = useState(true);
   const [includeSightWords, setIncludeSightWords] = useState(false);
   const [includeStates, setIncludeStates] = useState(false);
+  const [includeNovember2023, setIncludeNovember2023] = useState(false);
   const [pool, setPool] = useState(["2023", "Mandi", "Burley"]);
 
   const [glyph, setGlyph] = useState<string>("^_^");
 
   const updatePool = () => {
-    let pool = [];
+    let pool: string[] = [];
     if (includeNumbers) pool = [...pool, ...data.easyNumbers];
     if (includeUppercase) pool = [...pool, ...data.lettersUppercase];
     if (includeLowercase) pool = [...pool, ...data.lettersUppercase.map((letter) => letter.toLowerCase())];
     if (includeSightWords) pool = [...pool, ...data.sightWords];
     if (includeStates) pool = [...pool, ...data.states];
+    if (includeNovember2023) pool = [...pool, ...data.november2023];
     setPool(pool);
   }
 
@@ -52,7 +54,11 @@ function App() {
     setIncludeStates((prev) => !prev);
   };
 
-  function CustomTabPanel(props) {
+  const handleToggleNovember2023 = () => {
+    setIncludeNovember2023((prev) => !prev);
+  };
+
+  function CustomTabPanel(props: any) {
     const { children, value, index, ...other } = props;
     return (
       <div
@@ -71,7 +77,7 @@ function App() {
     );
   }
 
-  function a11yProps(index) {
+  function a11yProps(index: number) {
     return {
       id: `simple-tab-${index}`,
       'aria-controls': `simple-tabpanel-${index}`,
@@ -80,27 +86,22 @@ function App() {
 
   const [activeTab, setActiveTab] = useState(0);
 
-  const handleTabChange = (event, newValue) => {
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     setActiveTab(newValue);
   };
 
   return (
     <>
     <div className="app-container">
-        <Tabs value={activeTab} onChange={handleTabChange} aria-label="app-tabs">
+        <Tabs value={activeTab} onChange={handleTabChange} aria-label="app-tabs" className="tab-nav">
           <Tab label="memcards" {...a11yProps(0)} />
           <Tab label="settings" {...a11yProps(1)} />
         </Tabs>
 
         <CustomTabPanel value={activeTab} index={0}>
           <div className="mem-card-container">
-            <div className="glyph-container">
+            <div className="glyph-container" onClick={() => generateRandomGlyph()}>
               <div>{glyph}</div>
-            </div>
-            <div>
-              <Button variant="contained" size="large" className="heckinButton" onClick={() => generateRandomGlyph()}>
-                Generate a random glyph
-              </Button>
             </div>
           </div>
         </CustomTabPanel>
@@ -113,11 +114,12 @@ function App() {
               <FormControlLabel className="switch-item" control={<Switch checked={includeNumbers} onChange={() => handleToggleNumbers()} />} label="Numbers" />
               <FormControlLabel className="switch-item" control={<Switch checked={includeSightWords} onChange={() => handleToggleSightWords()} />} label="Sight Words" />
               <FormControlLabel className="switch-item" control={<Switch checked={includeStates} onChange={() => handleToggleStates()} />} label="U.S. States" />
+              <FormControlLabel className="switch-item" control={<Switch checked={includeNovember2023} onChange={() => handleToggleNovember2023()} />} label="November 2023" />
             </div>
           </div>
         </CustomTabPanel>
 
-        <div className="app-title">memcards v0.1</div>
+        <div className="app-title">memcards v0.2</div>
       </div>
     </>
   )
